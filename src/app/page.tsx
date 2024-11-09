7e06c299133893ffd5cf768264e20f8352abb7b6
anshuman619 <mishraanshuman619@gmail.com>
2024-11-09 20:14:26 +0530

    adding navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,44 @@ import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 
 import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { link } from "fs";
 
 export default function Home() {
+  let navItems = [
+    {
+      name: "Home",
+      link: "#home",
+    },
+    {
+      name: "Projects",
+      link: "#projects",
+    },
+    {
+      name: "About",
+      link: "#",
+    },
+    {
+      name: "Contact",
+      link: "#",
+    },
+  ];
+
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5  ">
       <div className="max-w-7xl w-full">
-        <Hero />
+        <FloatingNav
+          navItems={navItems}
+          className="m-auto bg-gradient-to-r from-[#04071d] to-[#0c0e23]  px-4 py-4"
+        ></FloatingNav>
+        <div id="home">
+          <Hero />
+        </div>
+
         <Grid />
-        <RecentProjects />
+        <div id="projects">
+          <RecentProjects />
+        </div>
       </div>
     </main>
   );
